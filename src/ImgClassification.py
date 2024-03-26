55ecbfcaf435d7e671c3e4db00eaaca1d440f323
Noor30903 <nolalwlaqi@effat.edu.sa>
2024-03-26 03:06:35 +0300

    refactoring the code
    automating the selection of the number of components plus fixing the code
    
    Co-Authored-By: Maha182 <148116369+maha182@users.noreply.github.com>

--- src/ImgClassification.py
+++ src/ImgClassification.py
@@ -15,6 +15,8 @@ from joblib import dump
 # constants
 imgHeight = 256
 imgWidth = 256
+TRAIN_DATASET_PATH = r'data\train_dataset'
+TEST_DATASET_PATH = r'data\test_dataset'
 
 labelCode = {
     'KA': 0,
@@ -33,16 +35,28 @@ labelCode = {
 def load_images(image_path, imgHeight, imgWidth, labelCode):
     labelsTrain = []
     img_vectorsTrain = []
+    imgname=[]
     for filepath in glob.glob(os.path.join(image_path, '*.jpg')):
-        labelsTrain.append(labelCode[filepath[19:21]])
+        labelsTrain.append(labelCode[filepath.split(os.sep)[-1][:2]])
         image = Image.open(filepath).convert('L')
+        fileName = filepath.split('\\')[2]
+        imgname.append(fileName)
         img_vectorsTrain.append(np.array(image).flatten().tolist())
-    return np.array(img_vectorsTrain), labelsTrain
+    return np.array(img_vectorsTrain), labelsTrain,imgname
 
-# RUN PCA
-nComp = 80
-pca = PCA(n_components=nComp, whiten=True).fit(np.array(img_vectorsTrain))
-eVals = np.square(pca.singular_values_)
+# Function to RUN PCA
+def apply_pca(X_train, variance_threshold=0.95):
+    
+    pca = PCA(n_components=variance_threshold, whiten=True)
+    X_train_pca = pca.fit_transform(X_train)
+    
+    # Determine the number of components that explain the threshold variance
+    n_components = np.sum(np.cumsum(pca.explained_variance_ratio_) <= variance_threshold)
+    print(f"PCA selected {n_components} components to explain {variance_threshold * 100}% of variance.")
+    
+    return pca, X_train_pca, n_components
+
+#eVals = np.square(pca.singular_values_)
 def plot_scree(eigenvalues):
     plt.figure('Scree Plot')
     plt.plot(np.arange(1, len(eigenvalues) + 1), eigenvalues, marker='o', linestyle='-', color='b')
@@ -54,104 +68,56 @@ def plot_scree(eigenvalues):
     plt.show()
 
 # find best k for knn classification
-def find_best_k(X_train, y_train, X_test, y_test, k_range, metric):
+def find_best_k(X_train, y_train, X_test, y_test, k_range, typeDist):
     accuracies = []
     for k in k_range:
-        clf = KNeighborsClassifier(n_neighbors=k, metric=metric)
+        clf = KNeighborsClassifier(n_neighbors=k, p=typeDist)
         clf.fit(X_train, y_train)
         pred = clf.predict(X_test)
         accuracies.append(metrics.accuracy_score(y_test, pred))
     best_k = k_range[np.argmax(accuracies)]
     print(f'Best k by accuracy: {best_k}')
+    plt.figure('K vs Accuracy')
+    plt.plot(k_range, accuracies, marker='o')
+    plt.xlabel('Number of Neighbors: k')
+    plt.ylabel('Accuracy')
+    plt.title('KNN Accuracy for different k values')
+    plt.grid(True)
+    plt.show()
+    return best_k
 
-    
-# literal 3: accumulated variance for chosen eigen vectors in 2) literal
-dictAccVariance = {}
-pcaVariance = pca.explained_variance_ratio_
-countVar = 0
-accVariance = 0
-accVarianceArr = []
-for var in pcaVariance:
-    countVar += 1
-    accVariance += var
-    accVarianceArr.append(accVariance)
-    dictAccVariance[f'PC-{countVar}'] = accVariance
-# print results
-strAccVariance = 'Accumulated variance for selected Principle Components\n\n'
-for k, v in dictAccVariance.items():
-    strAccVariance += f'{k} -> {v}\n'
-with open(r'data\results\AccVariance.txt', 'w', encoding="utf-8") as f:
-    f.write(strAccVariance)
-# plot accumulated variance
-plt.figure('Accumulated variance vs components')
-plt.subplot(111)
-plt.plot(range(1, nComp+1), accVarianceArr, marker='.')
-plt.hlines(0.95, 0, nComp, colors='r')
-plt.xlabel('Number of Components')
-plt.ylabel('Acc. Variance')
-plt.show()
+img_vectors_train, labels_train, imgnameTrain = load_images(TRAIN_DATASET_PATH, imgHeight, imgWidth, labelCode)
+
+pca, pca_imgVectorsTrain, n_components = apply_pca(np.array(img_vectors_train))
+
+img_vectors_test, labels_test, imgnameTest = load_images(TEST_DATASET_PATH, imgHeight, imgWidth, labelCode)
 
 # literal 6: Classification
-# transform train data to PCA, principle axis
-pca_imgVectorsTrain = pca.transform(np.array(imgVectorsTrain))
 typeDist = 2  # euclidean
-k = 2  # it was 8 we changed it to 2 for the best accuracy
+k_range=range(2, 20)
+k = find_best_k(img_vectors_train, labels_train, img_vectors_test, labels_test, k_range, typeDist)  # it was 8 we changed it to 2 for the best accuracy
+
+
 # build and train model that uses euclidean distance
 clf = KNeighborsClassifier(n_neighbors=k, p=typeDist)
-clf.fit(pca_imgVectorsTrain, labelsTrain)
-# go through test data set images
-imgVectorsTest = []
-testImgNames = []
-labelsTest = []
-for filepath in glob.glob(os.path.join(r'data\test_dataset', '*.jpg')):
-    # test correct labels
-    labelsTest.append(labelCode[filepath[18:20]])
-    # Create vector grayscale images
-    PIL_img = Image.open(filepath).convert('L')
-    # file shorter name
-    fileName = filepath.split('\\')[2]
-    testImgNames.append(fileName)
-    # width, height = PIL_img.size
-    imgVectorsTest.append(np.array(PIL_img).flatten().tolist())
-
+clf.fit(pca_imgVectorsTrain, labels_train)
 # transform test data with PCA
-pca_imgVectorsTest = pca.transform(np.array(imgVectorsTest))
-
+pca_imgVectorsTest = pca.transform(np.array(img_vectors_test))
 # predicted labels 
 pred_labels = clf.predict(pca_imgVectorsTest)
-
 print("Predictions from the classifier:")
 print(pred_labels)
 print("Target values:")
-print(labelsTest)
-
-
-typeDist = 2  # euclidean
-knnTestAcc = []
-for kValue in range(2, 20):
-    # build and train model that uses euclidean distance
-    clfTest = KNeighborsClassifier(n_neighbors=kValue, p=typeDist)
-    clfTest.fit(pca_imgVectorsTrain, labelsTrain)
-    # predicted labels
-    pred_labelsKnn = clfTest.predict(pca_imgVectorsTest)
-    knnTestAcc.append(metrics.accuracy_score(labelsTest, pred_labelsKnn))
-# plot best k
-plt.figure('K vs Accuracy')
-plt.subplot(111)
-plt.plot(range(2, 20), knnTestAcc, marker='.')
-plt.xlabel('K value')
-plt.ylabel('Accuracy')
-plt.show()
-
+print(labels_test)
 
 # results
 resultDict = defaultdict(list)
 invLabCode = dict((y, x) for x, y in labelCode.items())
-for label, fileName in zip(pred_labels, testImgNames):
+for label, fileName in zip(pred_labels, imgnameTest):
     resultDict[invLabCode[label]].append(fileName)
 # result dictionary to string
 strResult = 'PCA, to aid the process of image classification\n'
-strResult += f'Accuracy: {metrics.accuracy_score(labelsTest, pred_labels)}\n\n'
+strResult += f'Accuracy: {metrics.accuracy_score(labels_test, pred_labels)}\n\n'
 for key, val in resultDict.items():
     strResult += str(key) + '\n'
     for v in val:
@@ -172,8 +138,6 @@ with open(r'data\results\eigenVectMatrix.txt', 'w', encoding="utf-8") as f:
     for colNum in range(len(eigenVectMatrix)):
         pcColNames.append(f'PC{colNum}')
 
-
-
 # literal 1: Graph the eigen face of the mean of the initial matrix, before applyin PCA
 meanVect = pca.mean_
 plt.figure('Mean Vector Face')
@@ -184,14 +148,46 @@ plt.yticks(())
 #plt.show()
 
 # literal 3: Graph all eigen faces for selected principle components
+
 plt.figure('Eigen Faces')
 graphRows = 8
 graphCols = 10
-for i in range(graphRows * graphCols):
-    plt.subplot(graphRows, graphCols, i + 1)
-    plt.imshow(eigenVectMatrix[i].reshape(
-        (imgHeight, imgWidth)), cmap=plt.cm.gray)
-    # plt.title(f'PC-{i}')
+total_plots = min(graphRows * graphCols, n_components)  # Ensure we do not exceed the number of PCA components
+
+for i in range(total_plots):  # Use the adjusted total_plots here
+    plt.subplot(graphRows, graphCols, i+1)
+    plt.imshow(pca.components_[i].reshape((imgHeight, imgWidth)), cmap=plt.cm.gray)
     plt.xticks(())
     plt.yticks(())
 plt.show()
+
+
+
+
+
+
+# literal 3: accumulated variance for chosen eigen vectors in 2) literal
+#dictAccVariance = {}
+#pcaVariance = pca.explained_variance_ratio_
+#countVar = 0
+#accVariance = 0
+#accVarianceArr = []
+#for var in pcaVariance:
+#    countVar += 1
+#    accVariance += var
+#    accVarianceArr.append(accVariance)
+#    dictAccVariance[f'PC-{countVar}'] = accVariance
+## print results
+#strAccVariance = 'Accumulated variance for selected Principle Components\n\n'
+#for k, v in dictAccVariance.items():
+#    strAccVariance += f'{k} -> {v}\n'
+#with open(r'data\results\AccVariance.txt', 'w', encoding="utf-8") as f:
+#    f.write(strAccVariance)
+## plot accumulated variance
+#plt.figure('Accumulated variance vs components')
+#plt.subplot(111)
+#plt.plot(range(1, n_components+1), accVarianceArr, marker='.')
+#plt.hlines(0.95, 0, n_components, colors='r')
+#plt.xlabel('Number of Components')
+#plt.ylabel('Acc. Variance')
+#plt.show()
